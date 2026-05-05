34356e99b4fbfd09b00b7a1a9c93be6c57d43518
csgorun <dimitryi455@gmail.com>
2026-05-05 19:09:45 +0300

    learn dart

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = 'Артём';
+  String? name2 = null ;
+  var count = 0;
+  var title = 'Учить Rust';
+  var x = 5;
+  x = 'text';
+  final id = 1;
+  const appName = 'TodoApp';
+}
